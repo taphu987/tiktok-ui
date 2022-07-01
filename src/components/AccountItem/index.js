2be02c3c31d8200ef0e22f14daad4f5ca63fbf95
taphu987 <phu.tn1309@gmail.com>
2022-07-01 15:19:48 +0700

    Xây dựng UI phần Header #7

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,13 +2,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import classNames from "classnames/bind";
 import styles from "./AccountItem.module.scss";
+import Image from "../images";
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx("wrapper")}>
-            <img
+            <Image
                 className={cx("avatar")}
                 src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/31a2e92512606aa596df657166e5e483~c5_300x300.webp?x-expires=1656781200&x-signature=05tmcdElV%2Bg6%2FZpnZNDPv6hlxgQ%3D"
                 alt="Thao"
